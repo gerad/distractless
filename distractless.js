f854e2f58c8d4694246ec85b0fbbbab423df428f
Gerad Suyderhoud <gerads@gmail.com>
2014-02-23 07:14:44 -0800

    more distracting sites

--- distractless.js
+++ distractless.js
@@ -3,4 +3,7 @@ chrome.webNavigation.onBeforeNavigate.addListener(function(details) {
 }, { url: [
   { hostEquals: 'news.ycombinator.com' },
   { hostEquals: 'news.google.com' },
+  { hostEquals: 'grantland.com' },
+  { hostEquals: 'deadspin.com' },
+  { hostEquals: 'bleacherreport.com' },
 ]});
